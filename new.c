1679c9fa6a9cd2ec8b5ba3ee12b7fc2e002a421f
Elvin Mammadli <35107186+ElvinMammadli@users.noreply.github.com>
2018-01-07 05:39:06 +0300

    Add files via upload

--- new.c
+++ new.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#define PORT 8560
+#define PORT 8540
 
 #define clear() printf("\033[H\033[J")
 
@@ -35,9 +35,8 @@ int main(int argc, char const *argv[])
     int a;
     struct sockaddr_in serv_addr;
     char hello[100] ;
-    char buffer[1024] ;
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
+    char buffer[1024]={0} ;
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
         printf("\n Socket creation error \n");
         return -1;
     }
@@ -49,105 +48,88 @@ int main(int argc, char const *argv[])
     serv_addr.sin_addr.s_addr=INADDR_ANY;
 
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
         printf("\nConnection Failed \n");
         return -1;
     }
-    strcpy(yeni.id, argv[1]);
+   strcpy(yeni.id, argv[1]);
    printf("%s Kullanicisi icin giris yaptiniz\n",yeni.id);
    fp = fopen ("txt/kullanicilar.txt", "r+");
    fscanf(fp,"%s %s %d %s",comp_id,name,&num,mail1);
 
    while (!feof(fp)){
      //printf("%s %s %d %s\n",comp_id,name,num,mail1 );
-     if (strcmp(comp_id, yeni.id) == 0){
-       printf("Kullanici bulunmaktadir");
-       tmp=1;
-       break;
-     }
-     fscanf(fp,"%s %s %d %s",comp_id,name,&num,mail1);
+      if (strcmp(comp_id, yeni.id) == 0){
+        printf("Kullanici bulunmaktadir");
+        tmp=1;
+        break;
+      }
+      fscanf(fp,"%s %s %d %s",comp_id,name,&num,mail1);
 
    }
    if(tmp==0){
-     printf("Boyle kullanici bulunmamaktadir\n");
-     printf("Kayit olmak icin isminizi , telefon numaranizi ve mailinizi sirasiyla giriniz\n");
-     scanf("%s %d %s",yeni.isim,&yeni.numara,yeni.mail );
+      printf("Boyle kullanici bulunmamaktadir\n");
+      printf("Kayit olmak icin isminizi , telefon numaranizi ve mailinizi sirasiyla giriniz\n");
+      scanf("%s %d %s",yeni.isim,&yeni.numara,yeni.mail );
 
-     int i;
-     char* extension;//="gelen.txt";
-     char fileSpec[32];
-     FILE *out;
+      int i;
+      char* extension;//="gelen.txt";
+      char fileSpec[32];
+      FILE *out;
 
-     snprintf( fileSpec, sizeof( fileSpec ), "txt/%sgelen.txt", yeni.id );
-     out = fopen( fileSpec, "w" );
-     fclose(out);
-     snprintf( fileSpec, sizeof( fileSpec ), "txt/%sgiden.txt", yeni.id );
-     out  = fopen( fileSpec, "w" );
-     fclose(out);
+      snprintf( fileSpec, sizeof( fileSpec ), "txt/%skonusma.txt", yeni.id );
+      out = fopen( fileSpec, "w" );
+      fclose(out);
 
-     snprintf( fileSpec, sizeof( fileSpec ), "txt/%slist.txt", yeni.id );
-     out = fopen( fileSpec, "w" );
-     fclose(out);
-     fprintf(fp, "%s %s %d %s\n",yeni.id,yeni.isim,yeni.numara,yeni.mail);
-     fclose(fp);
+      snprintf( fileSpec, sizeof( fileSpec ), "txt/%slist.txt", yeni.id );
+      out = fopen( fileSpec, "w" );
+      fclose(out);
+      fprintf(fp, "%s %s %d %s\n",yeni.id,yeni.isim,yeni.numara,yeni.mail);
+      fclose(fp);
   }
   printf("Kullanici bilgilerinize bakmak istermisiniz?" );
   printf("\n\n\n\n\n\n" );
   if (tmp==1) {
-    printf("%s %s  %d  %s\n",comp_id,name,num,mail1 );
+      printf("%s %s  %d  %s\n",comp_id,name,num,mail1 );
   }
   else{
-    printf("%s %s  %d  %s\n",yeni.id,yeni.isim,yeni.numara,yeni.mail );
- }
-    printf("106\n");
+      printf("%s %s  %d  %s\n",yeni.id,yeni.isim,yeni.numara,yeni.mail );
+  }
     printf("Write your message\n");
     scanf(" %[^\n]%*c",hello);
 
     printf("kime mesaj gondermek istiyorsunuz?\n" );
-    scanf(" %s",kime);
-    send(sock,hello,strlen(hello),0);
-    send(sock,kime,strlen(kime),0);
-    send(sock,yeni.id,strlen(yeni.id),0);
+    scanf("%s",kime);
 
-    /*snprintf( fileSpec, sizeof( fileSpec ), "txt/%slist.txt", yeni.id );
+    strcat(buffer,kime);
+    strcat(buffer," ");
+    strcat(buffer,yeni.id);
+    strcat(buffer," ");
+    strcat(buffer,hello);
+    int flag=0;
+    snprintf( fileSpec, sizeof( fileSpec ), "txt/%slist.txt", yeni.id );
     out = fopen( fileSpec, "a+" );
     while (!feof(out)) {
-      fscanf(out,"%s",list );
-      if (strcmp(list,kime)) {
-          printf("Kullanici listenizde bulunmamaktadir,kisiyi listenize eklemek istiyorsaniz 1 e basiniz\n" );
-          scanf("%d",&d );
-            if (d==1) {
-              fprintf(out, "%s\n",kime );
-              send(sock,yeni.id,strlen(yeni.id),0);
-              printf("Mesajiniz gonderildi");
-
-            }
-     }
-      else{
-          send(sock , hello , strlen(hello) , 0 );
-          send(sock,kime,strlen(kime),0);
+        fscanf(out,"%s",list );
+        a=strcmp(list,kime);
+        if (a==0) {
+          flag=1;
+          send(sock,buffer,strlen(buffer),0);
           printf("Kullanici listenizde bulunuyor,mesajiniz %s kisisine basariyla gonderildi\n",kime );
       }
-      }
+    }
+    if(flag!=1){
+          printf("Kullanici listenizde bulunmamaktadir,kisiyi listenize eklemek istiyorsaniz 1 e basiniz\n" );
+          scanf("%d",&d );
+          if (d==1) {
+              fprintf(out, "%s\n",kime );
+              send(sock,buffer,strlen(buffer),0);
+              printf("%s kullanicisi listenize eklendi\n",kime );
+              printf("Mesajiniz gonderildi");
+          }
+    }
 
     fclose(out);
-*/
 
-
-  /*  snprintf( fileSpec, sizeof( fileSpec ), "txt/%sgelen.txt", yeni.id );
-    out = fopen( fileSpec, "a+" );
-    fprintf(out,"%s %s\n",kimden,hello );
-    fclose(out);
-    snprintf( fileSpec, sizeof( fileSpec ), "txt/%sgiden.txt", yeni.id );
-    out  = fopen( fileSpec, "a+" );
-    fprintf(out, "%s %s\n",kime,hello );
-    fclose(out);
-*/
-        //while(1){
-      //valread = read( sock , buffer, 1024);
-      //recv(sock,&buffer,sizeof(buffer),0);
-      //printf("%s\n",buffer );
-      //printf("\n" );
     return 0;
 }
--- serve.c
+++ serve.c
@@ -4,99 +4,107 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-#define PORT 8560
+#define PORT 8540
 
 void *connection_handler(void *);
 
 FILE *out,*fp;
 char fileSpec[32];
-
 struct bilgi{
   char buffer[1024] ;
-  char  hello[60];
   char kime[5];
   char kimden[5];
 };
 int server_fd, new_socket, valread,b;
-
+char hello[60];
+char apple[1024];
 int main(int argc, char const *argv[])
 {
-      struct sockaddr_in address;
-      int opt = 1,c;
-      int addrlen = sizeof(address);
+  struct sockaddr_in address;
+  int opt = 1,c=0;
+  int addrlen = sizeof(address);
 
     // Creating socket file descriptor
-      if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-      {
+  if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
           perror("socket failed");
           exit(EXIT_FAILURE);
-      }
+  }
 
     // Forcefully attaching socket to the port 8080
-      if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt)))
-      {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,&opt, sizeof(opt))){
         perror("setsockopt");
         exit(EXIT_FAILURE);
-      }
+  }
       address.sin_family = AF_INET;
       address.sin_addr.s_addr = INADDR_ANY;
       address.sin_port = htons( PORT );
 
-    // Forcefully attaching socket to the port 8080
-      if (bind(server_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_in))<0)
-      {
+    // Forcefully attaching sock%set to the port 8080
+   if (bind(server_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_in))<0){
         perror("bind failed");
         exit(EXIT_FAILURE);
-      }
- c = sizeof(struct sockaddr_in);
-      if (listen(server_fd, 3) < 0)
-      {
+   }
+
+   if (listen(server_fd, 3) < 0){
         perror("listen");
         exit(EXIT_FAILURE);
-      }
-      pthread_t  thread_id;
-      while( (new_socket= accept(server_fd, (struct sockaddr *)&address, &c) ))
-      {
+   }
+  pthread_t  thread_id;
+  while( (new_socket= accept(server_fd, (struct sockaddr *)&address, &c) )){
          puts("Connection accepted");
 
-          if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &new_socket) < 0)
-          {
+          if( pthread_create( &thread_id , NULL ,  connection_handler , (void*) &new_socket) < 0){
              perror("could not create thread");
              return 1;
           }
-      }
-      return 0;
+  }
+  return 0;
 }
+
+
+
 void *connection_handler(void *socket_desc)
 {
 
-  struct bilgi insan,insan1;
+  struct bilgi insan;
+  memset(insan.buffer, 0, sizeof(insan.buffer));
   //  int sock = *(int*)socket_desc;
 
-    recv(new_socket,&insan.buffer,sizeof(insan.buffer),0);
-    recv(new_socket,&insan.kime,sizeof(insan.kime),0);
-    recv(new_socket,&insan.kimden,sizeof(insan.kimden),0);
-    //printf("Kimden\n" );
-    printf("%s\t",insan.kime );
-    printf("%s\t",insan.buffer );
-    printf("%s\t",insan.kimden );
+  recv(new_socket,&apple,sizeof(apple),0);
 
-    snprintf( fileSpec, sizeof( fileSpec ), "txt/%sgelen.txt", insan.kime );
+    int i=0,c=0;
+    while(apple[c]!=' '){
+        insan.kime[c]=apple[c];
+        c++;
+    }
+    insan.kime[c]='\0';
+    c++;
+    while (apple[c]!=' ') {
+      insan.kimden[i]=apple[c];
+      i++;
+      c++;
+    }
+    insan.kimden[i]='\0';
+    i=0;
+    while (apple[c]!='\0') {
+      insan.buffer[i]=apple[c];
+      i++;
+      c++;
+    }
+    insan.buffer[i]='\0';
+    printf("%s\n",insan.kime );
+    printf("%s\n",insan.kimden );
+    printf("%s\n",insan.buffer );
+
+    char apple='\0';
+    snprintf( fileSpec, sizeof( fileSpec ), "txt/%skonusma.txt", insan.kime );
     out = fopen( fileSpec, "a+" );
-    fwrite(&insan,sizeof(struct bilgi),1,out);
+    fprintf(out, "+ %s %s\n",insan.kimden,insan.buffer );
     fclose(out);
 
-    out = fopen( fileSpec, "r+" );
-    printf("%s\n",fileSpec );
-    printf("%s\n %s\n %s\n ",insan1.kimden,insan1.kime, insan1.buffer );
-    fread(&insan1,sizeof(struct bilgi),1,out);
-    fclose(out);
-
-    printf("4n4n\n" );
-
-    snprintf( fileSpec, sizeof( fileSpec ), "txt/%sgiden.txt", insan.kimden );
+    snprintf( fileSpec, sizeof( fileSpec ), "txt/%skonusma.txt", insan.kimden );
     fp  = fopen( fileSpec, "a+" );
-    fwrite(&insan,sizeof(struct bilgi),1,out);
+    fprintf(fp, "- %s %s\n",insan.kime,insan.buffer );
     fclose(fp);
 
     //printf("Write your message\n");
